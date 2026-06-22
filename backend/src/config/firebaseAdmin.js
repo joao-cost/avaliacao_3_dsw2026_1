68b09b45f33ab1ad374fd769132def931cbe0e6f
João Vitor <joaovitor00220@gmail.com>
2026-06-22 15:28:08 -0400

    Ajuste de chave firebase

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -9,7 +9,7 @@ async function obterServiceAccount() {
     return {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/^["']|["']$/g, '').replace(/\\n/g, '\n'),
     };
   }
 
